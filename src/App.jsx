a1f775aed9184cfd72ef6f1da0791275388892cc
Aquilesmickeymoyse <rm94718@estudante.fieb.edu.br>
2025-03-19 08:27:52 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,24 @@ import { useState } from "react"
 function app(){
 
   const [nome, setNome] = useState('Ronaldo')
+
+  const Formulario = () => {
+
+    return (
+    <div>
+         <input 
+       className="nome"
+       onChange={(e)=>{setNome(e.target.value)}}
+       name="nome"
+       type="text" />
+       <button
+        onClick={()=>{alert(nome)}}
+        className="Botão">
+        CLIQUE AQUI
+       </button>
+    </div>
+    )
+  }
   return (
     <div>
        <h3> Pizzaria 2C</h3>
@@ -19,5 +37,15 @@ function app(){
     </div>
   )
 }
+
+return (
+  <div>
+    <h3> Pizzaria 2C </h3>
+    <Formulario/>
+    <Formulario/>
+    <Formulario/>
+  </div>
+)
+
 
 export default app
